a765640d9313a0e97ef2f6825c016d5adcd0b39b
phongdeeptry88 <phong882k2@gmail.com>
2022-05-08 16:07:17 +0700

    phong kkcodekk

--- pbl/View/Admin/Schedule.Designer.cs
+++ pbl/View/Admin/Schedule.Designer.cs
@@ -29,22 +29,17 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.cbbgadi = new System.Windows.Forms.ComboBox();
+            this.cbbgaden = new System.Windows.Forms.ComboBox();
+            this.thoigiandi = new System.Windows.Forms.DateTimePicker();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.btaddschedule = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.comboBox4 = new System.Windows.Forms.ComboBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.thoigianden = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -58,43 +53,45 @@
             this.dataGridView1.Size = new System.Drawing.Size(835, 287);
             this.dataGridView1.TabIndex = 0;
             // 
-            // comboBox1
+            // cbbgadi
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(130, 26);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 1;
+            this.cbbgadi.FormattingEnabled = true;
+            this.cbbgadi.Location = new System.Drawing.Point(130, 26);
+            this.cbbgadi.Name = "cbbgadi";
+            this.cbbgadi.Size = new System.Drawing.Size(121, 24);
+            this.cbbgadi.TabIndex = 1;
             // 
-            // comboBox2
+            // cbbgaden
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(383, 26);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 24);
-            this.comboBox2.TabIndex = 2;
+            this.cbbgaden.FormattingEnabled = true;
+            this.cbbgaden.Location = new System.Drawing.Point(130, 75);
+            this.cbbgaden.Name = "cbbgaden";
+            this.cbbgaden.Size = new System.Drawing.Size(121, 24);
+            this.cbbgaden.TabIndex = 2;
             // 
-            // dateTimePicker1
+            // thoigiandi
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(383, 81);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(240, 22);
-            this.dateTimePicker1.TabIndex = 3;
+            this.thoigiandi.CustomFormat = "dd/MM/yyyy HH:mm";
+            this.thoigiandi.Location = new System.Drawing.Point(446, 73);
+            this.thoigiandi.Name = "thoigiandi";
+            this.thoigiandi.Size = new System.Drawing.Size(240, 22);
+            this.thoigiandi.TabIndex = 3;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Cyan;
-            this.button1.Location = new System.Drawing.Point(729, 72);
+            this.button1.Location = new System.Drawing.Point(745, 61);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(139, 38);
             this.button1.TabIndex = 4;
             this.button1.Text = "TÌM KIẾM";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(298, 83);
+            this.label1.Location = new System.Drawing.Point(349, 29);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(66, 16);
             this.label1.TabIndex = 5;
@@ -103,7 +100,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(298, 29);
+            this.label2.Location = new System.Drawing.Point(46, 83);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(63, 16);
             this.label2.TabIndex = 6;
@@ -139,57 +136,6 @@
             this.btaddschedule.UseVisualStyleBackColor = false;
             this.btaddschedule.Click += new System.EventHandler(this.btaddschedule_Click);
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(576, 29);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(41, 16);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "TÀU :";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(623, 26);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(121, 22);
-            this.textBox1.TabIndex = 12;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(46, 86);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(51, 16);
-            this.label5.TabIndex = 13;
-            this.label5.Text = "GIỜ ĐI :";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(167, 86);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(11, 16);
-            this.label6.TabIndex = 16;
-            this.label6.Text = ":";
-            // 
-            // comboBox3
-            // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(184, 83);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(31, 24);
-            this.comboBox3.TabIndex = 17;
-            // 
-            // comboBox4
-            // 
-            this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(130, 83);
-            this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(31, 24);
-            this.comboBox4.TabIndex = 18;
-            // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.Cyan;
@@ -200,28 +146,31 @@
             this.button2.Text = "Thoát";
             this.button2.UseVisualStyleBackColor = false;
             // 
+            // thoigianden
+            // 
+            this.thoigianden.CustomFormat = "dd/MM/yyyy HH:mm";
+            this.thoigianden.Location = new System.Drawing.Point(446, 23);
+            this.thoigianden.Name = "thoigianden";
+            this.thoigianden.Size = new System.Drawing.Size(240, 22);
+            this.thoigianden.TabIndex = 20;
+            // 
             // Schedule
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(953, 606);
+            this.Controls.Add(this.thoigianden);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.comboBox4);
-            this.Controls.Add(this.comboBox3);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.btaddschedule);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.thoigiandi);
+            this.Controls.Add(this.cbbgaden);
+            this.Controls.Add(this.cbbgadi);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Schedule";
             this.Text = "Schedule";
@@ -234,21 +183,16 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox cbbgadi;
+        private System.Windows.Forms.ComboBox cbbgaden;
+        private System.Windows.Forms.DateTimePicker thoigiandi;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button btaddschedule;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.ComboBox comboBox3;
-        private System.Windows.Forms.ComboBox comboBox4;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DateTimePicker thoigianden;
     }
 }
